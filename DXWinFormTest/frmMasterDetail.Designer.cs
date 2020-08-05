7129bbdee4bf9c2acf77bd2ecd55ad3c3cfb3095
BobinYang <xiang-ai-2002@163.com>
2020-08-05 13:41:35 +0800

    “注释”

---
+++ DXWinFormTest/frmMasterDetail.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DXWinFormTest
+{
+    partial class frmMasterDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraGrid.GridLevelNode gridLevelNode1 = new DevExpress.XtraGrid.GridLevelNode();
+            this.gridControl = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl
+            // 
+            this.gridControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            gridLevelNode1.LevelTemplate = this.gridView2;
+            gridLevelNode1.RelationName = "Detail2List";
+            this.gridControl.LevelTree.Nodes.AddRange(new DevExpress.XtraGrid.GridLevelNode[] {
+            gridLevelNode1});
+            this.gridControl.Location = new System.Drawing.Point(0, 0);
+            this.gridControl.MainView = this.gridView1;
+            this.gridControl.Name = "gridControl";
+            this.gridControl.Size = new System.Drawing.Size(1100, 700);
+            this.gridControl.TabIndex = 0;
+            this.gridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1,
+            this.gridView2});
+            // 
+            // gridView1
+            // 
+            this.gridView1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn3,
+            this.gridColumn4});
+            this.gridView1.GridControl = this.gridControl;
+            this.gridView1.Name = "gridView1";
+            // 
+            // gridView2
+            // 
+            this.gridView2.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2});
+            this.gridView2.GridControl = this.gridControl;
+            this.gridView2.Name = "gridView2";
+            this.gridView2.ViewCaption = "详情表";
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "从表名称";
+            this.gridColumn1.FieldName = "Name";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "从表描述";
+            this.gridColumn2.FieldName = "Description";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "主表名称";
+            this.gridColumn3.FieldName = "Name";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 0;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "主表描述";
+            this.gridColumn4.FieldName = "Description";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 700);
+            this.Controls.Add(this.gridControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+
+    }
+}
---
+++ DXWinFormTest/frmMasterDetail.cs
@@ -0,0 +1,114 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+
+namespace DXWinFormTest
+{
+    public partial class frmMasterDetail : XtraForm
+    {
+        public frmMasterDetail()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 绑定数据源
+        /// </summary>
+        private void BindData()
+        {
+            //创建测试数据
+            var result1 = new Detail2Result()
+            {
+                Name = "测试1",
+                Description = "描述内容",
+                Detail2List = new List<DetailInfo>()
+                {
+                    new DetailInfo()
+                    {
+                        Name = "111测试",
+                        Description = "111描述内容"
+                    },
+                    new DetailInfo()
+                    {
+                        Name = "222测试",
+                        Description = "222描述内容"
+                    },
+                    new DetailInfo()
+                    {
+                        Name = "333测试",
+                        Description = "333描述内容"
+                    }
+                }
+            };
+
+            var result2 = new Detail2Result()
+            {
+                Name = "测试2",
+                Description = "描述内容",
+                Detail2List = new List<DetailInfo>()
+                {
+                    new DetailInfo()
+                    {
+                        Name = "111测试",
+                        Description = "111描述内容"
+                    },
+                    new DetailInfo()
+                    {
+                        Name = "222测试",
+                        Description = "222描述内容"
+                    },
+                    new DetailInfo()
+                    {
+                        Name = "333测试",
+                        Description = "333描述内容"
+                    }
+                }
+            };
+
+            //构造一个记录的集合
+            var list = new List<Detail2Result>() { result1, result2 };
+
+            //绑定数据源
+            this.gridControl.DataSource = list;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindData();
+        }
+    }
+
+    /// <summary>
+    /// 记录基础信息
+    /// </summary>
+    public class DetailInfo
+    {
+        public DetailInfo()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// ID标识
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 描述信息
+        /// </summary>
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// 二级层次的列表
+    /// </summary>
+    public class Detail2Result : DetailInfo
+    {
+        public List<DetailInfo> Detail2List { get; set; }
+    }
+}
